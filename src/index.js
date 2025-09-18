5161825b5bddc07c30004777ba6ca35b445b9df6
l <eh.chahkiev@smartics.ru>
2025-09-18 16:38:50 +0300

    fix: build

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import { default as iso } from './iso'
 import { default as utc } from './utc'
 import { default as sql } from './sql'
 
-export {
-  iso,
-  utc,
-  sql,
-}
-
 export default {
   iso,
   utc,
